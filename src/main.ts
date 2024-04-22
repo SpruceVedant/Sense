5760c0da021c65a6215cdcb01efcd619a7ccbd10
Vedant Bakshi <vedantbakshi07@gmail.com>
2024-04-22 23:56:23 +0530

    Added config function

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+export function getConfig() {
+    const config = vscode.workspace.getConfiguration("SuiteSense");
+
+    const sites = {
+        "stackoverflow.com": config.settings.sites.stackoverflow,
+        "gist.github.com": config.settings.sites.githubGist
+    };
+
+    return {
+        settings: {
+            sites,
+            maxResults: config.settings.maxResults
+        }
+    } as IConfig;
+}
